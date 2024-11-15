f84ed7b133f254854d8382c02c837bd2710bf10d
faizyi <faizi13062000@gmail.com>
2024-11-15 07:33:16 +0500

    link

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,22 +1,22 @@
 // StateFull
 // const userSessionId = new Map();
-// import dotenv from "dotenv"
-// dotenv.config();
+import dotenv from "dotenv"
+dotenv.config();
 import jwt from "jsonwebtoken";
-const SECRET_KEY = "bhjyfdvcvcvcftdyudyugediuiasjbhbhjvadowyddfvavsgh"
+// const SECRET_KEY = "bhjyfdvcvcvcftdyudyugediuiasjbhbhjvadowyddfvavsgh"
 
 export const setUser = (user)=>{
     return jwt.sign({
         _id: user._id,
         email: user.email,
         role: user.role
-    }, SECRET_KEY)
+    }, process.env.SECRET_KEY)
 }
 
 export const getUser = (token)=>{
     if(!token) return null;
     try {
-        return jwt.verify(token, SECRET_KEY) 
+        return jwt.verify(token, process.env.SECRET_KEY) 
     } catch (error) {
         return null;
     }
